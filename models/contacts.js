f98172a1c1186658832b1b94e4d572a9f1384366
mariiamlyuzan <86401047+mariiamlyuzan@users.noreply.github.com>
2022-05-21 22:21:44 +0300

    hw02

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,140 @@
-// const fs = require('fs/promises')
+const path = require("path");
+const fs = require("fs").promises;
+const contactsPath = path.resolve("models/contacts.json");
+const shortid = require("shortid");
 
-const listContacts = async () => {}
+async function getContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-const getContactById = async (contactId) => {}
+async function listContacts(req, res, next) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    const contacts = JSON.parse(data);
+    return res.json({ status: "success", code: 200, contacts });
+  } catch (error) {
+    next(error);
+  }
+}
 
-const removeContact = async (contactId) => {}
+async function getContactById(req, res, next) {
+  try {
+    const { contactId } = req.params;
+    const contacts = await getContacts();
 
-const addContact = async (body) => {}
+    const contact = contacts.find(
+      (contact) => contact.id === contactId.toString()
+    );
+    console.log(contact);
+    if (!contact) {
+      const error = new Error(`No contact with ${contactId} id`);
+      error.status = 404;
+      throw error;
+    }
 
-const updateContact = async (contactId, body) => {}
+    return res.json({ status: "success", code: 200, contact });
+  } catch (error) {
+    next(error);
+  }
+}
 
+async function removeContact(req, res, next) {
+  try {
+    const contacts = await getContacts();
+    const { contactId } = req.params;
+
+    const newContacts = contacts.filter(
+      (contact) => contact.id !== contactId.toString()
+    );
+    const contact = contacts.find(
+      (contact) => contact.id === contactId.toString()
+    );
+
+    if (!contact) {
+      const error = new Error(`No contact with ${contactId} id`);
+      error.status = 404;
+      throw error;
+    }
+    await fs.writeFile(
+      "models/contacts.json",
+      JSON.stringify(newContacts),
+      "utf8"
+    );
+
+    return res.json({
+      status: "success",
+      code: 200,
+      mesasage: "contact deleted",
+      contact,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function addContact(req, res, next) {
+  try {
+    const contacts = await getContacts();
+
+    const { name, email, phone } = req.body;
+
+    contacts.push({ id: shortid.generate(), name, email, phone });
+
+    await fs.writeFile(
+      "models/contacts.json",
+      JSON.stringify(contacts),
+      "utf8"
+    );
+
+    res.status(201).json({ status: "success", code: 201, contacts });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function updateContact(req, res, next) {
+  try {
+    const { name, email, phone } = req.body;
+
+    const contacts = await getContacts();
+
+    const { contactId } = req.params;
+
+    const contact = contacts.find(
+      (contact) => contact.id === contactId.toString()
+    );
+
+    if (!contact) {
+      const error = new Error(`No contact with ${contactId} id`);
+      error.status = 404;
+      throw error;
+    }
+    contacts.forEach((contact) => {
+      if (contact.id === contactId.toString()) {
+        contact.name = name;
+        contact.email = email;
+        contact.phone = phone;
+      }
+    });
+    await fs.writeFile(
+      "models/contacts.json",
+      JSON.stringify(contacts),
+      "utf8"
+    );
+    res.json({ status: "success", code: 200, contacts });
+  } catch (error) {
+    next(error);
+  }
+}
 module.exports = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
   updateContact,
-}
+};
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const app = require('./app')
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+app.listen(3000, (error) => {
+  if (error) console.log("Error server", error);
+  console.log("Server running. Use our API on port: 3000");
+});
